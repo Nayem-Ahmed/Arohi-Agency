47c4db82c6d59a97fd2b67cc9b77987c171893ae
unknown <freelancernayem.me@gmail.comm>
2024-05-07 17:05:17 +0600

    navigation

--- src/MainlayOut/Root.jsx
+++ src/MainlayOut/Root.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../Components/Navbar';
 
 const Root = () => {
     return (
-        <div>
-            <Outlet></Outlet>
+        <div className='max-w-screen-xl mx-auto'>
+            <Navbar></Navbar>
+            <div className='min-h-[calc(100vh-60px)]'>
+                <Outlet/>
+            </div>
         </div>
     );
 };
